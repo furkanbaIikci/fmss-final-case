79999cdcc06759c6ab354a3879d1faa4c1f58be8
Furkan <furkan_blkc@hotmail.com>
2023-04-12 13:53:14 +0300

    create project and librarys

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+	return <div className='App'>
+    
+    <div className="text-3xl bg-blue-700">
+      Hello World
+    </div>
+
+
+  </div>;
+}
+
+export default App;
